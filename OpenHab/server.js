1745bf0d233774a59076564f30900956fb892dcf
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2023-11-28 16:41:09 +0100

    [DEV] new endpoint

--- OpenHab/server.js
+++ OpenHab/server.js
@@ -8,13 +8,27 @@ const OPENHAB_IP = 'http://openhab.ubiquarium.fr';
 
 app.get('/data', async (req, res) => {
     try {
-        const response = await axios.get(`${OPENHAB_IP}/rest/`);
+        const response = await axios.get(`${OPENHAB_IP}/rest/items`);
+        console.log('reponse is :',response.data)
         res.json(response.data);
     } catch (error) {
         console.error('Error fetching data from OpenHAB:', error);
         res.status(500).send('Error fetching data from OpenHAB');
     }
 });
+
+app.get('/status-door4', async (req, res) => {
+    try {
+        const response = await axios.get(`${OPENHAB_IP}/rest/items/MultiSensorDoor04_Sensor_Door`);
+        console.log('reponse is :',response)
+        res.json(response);
+    } catch (error) {
+        console.error('Error fetching data from OpenHAB:', error);
+        res.status(500).send('Error fetching data from OpenHAB');
+    }
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
